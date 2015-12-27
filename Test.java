66129ac1080878ea8684870bd0c400d58a390319
Mohammadreza Moghimi <rezaufo1234@gmail.com>
2015-12-27 13:11:14 +0330

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+/**mim series Code number 1.1
+programmer name : mimr moghimi
+This program can display a text about a math line then counting it's result*/
+public class Test
+{
+	/**main method*/
+	public static void main(String[] args)
+	{
+		//display a text
+		System.out.println("3.5 * 4/2 - 2.5 is");
+		//display result of above text mean
+		System.out.println(3.5*4/2-2.5);
+	}
+}
